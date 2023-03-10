67f9eafa9f23dd43cdce99360d80efc49dfbd505
johnkillswitch <johnkillswitch9@gmail.com>
2023-03-10 21:51:23 +0300

    хуйню сделал

--- src/main/kotlin/me/xezard/devmc/drazex/discord/DrazexBot.kt
+++ src/main/kotlin/me/xezard/devmc/drazex/discord/DrazexBot.kt
@@ -1,11 +1,18 @@
 package me.xezard.devmc.drazex.discord
 
 import discord4j.core.DiscordClient
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.event.domain.message.MessageCreateEvent
+import discord4j.core.`object`.entity.User
+import discord4j.core.`object`.entity.channel.MessageChannel
 import discord4j.core.`object`.presence.ClientActivity
 import discord4j.core.`object`.presence.ClientPresence
 import jakarta.annotation.PostConstruct
+import me.xezard.devmc.drazex.discord.commands.listeners.CommandRegistrator
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 
 @Component
 class DrazexBot(
@@ -23,8 +30,25 @@ class DrazexBot(
     @PostConstruct
     fun init() {
         this.discord = DiscordClient.create(this.botToken)
-
         this.discord.gateway()
-            .setInitialPresence { ClientPresence.online(ClientActivity.playing("Minecraft")) }
+            .setInitialPresence {
+                ClientPresence.online(ClientActivity.playing("Minecraft"))
+            }.login().subscribe()
+        this.discord.withGateway { gateway: GatewayDiscordClient ->
+            val printOnLogin: Mono<Void> = gateway.on(ReadyEvent::class.java) { event ->
+                    Mono.fromRunnable<Void>() {
+                        val self: User = event.self
+                        System.out.printf(
+                            "Logged in as %s#%s%n",
+                            self.username,
+                            self.discriminator
+                        )
+                    }
+            }
+                .then()
+
+            return@withGateway printOnLogin
+        }.subscribe()
+
     }
 }
