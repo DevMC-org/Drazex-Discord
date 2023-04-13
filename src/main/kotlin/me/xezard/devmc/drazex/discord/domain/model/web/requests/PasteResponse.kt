70c6feeae1797c3316ba6940d7daea488db6696c
johnkillswitch <johnkillswitch9@gmail.com>
2023-04-13 23:36:00 +0300

    хуйню сделал

---
+++ src/main/kotlin/me/xezard/devmc/drazex/discord/domain/model/web/requests/PasteResponse.kt
@@ -0,0 +1,13 @@
+package me.xezard.devmc.drazex.discord.domain.model.web.requests
+
+import me.xezard.devmc.drazex.discord.domain.model.web.requests.Result
+
+class PasteResponse (
+        var result: Result
+)
+class Result (
+    var files: List<FileResponse>
+)
+class FileResponse (
+    var id: String
+)
